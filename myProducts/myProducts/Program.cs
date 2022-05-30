37032da4fe9f9c5e3a99c1ac550274ac589570cd
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-05-30 15:51:42 +0300

    local connection string added

--- myProducts/myProducts/Program.cs
+++ myProducts/myProducts/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using myProducts.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ProductDbContext>(option => option.UseSqlServer(""));
+    
+     
 
 var app = builder.Build();
 
