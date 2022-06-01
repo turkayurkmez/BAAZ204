7002c80740d7c3ea338e7b39ef2101745c6c78a7
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-06-01 16:47:05 +0300

    Cosmos with EF

---
+++ CosmosWithEF/CosmosWithEF/Data/ProductDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CosmosWithEF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CosmosWithEF.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseCosmos("https://turka-cosmos.documents.azure.com:443/", "TwT2LpE5hJmvDrViEnpsEijKd5doXnCTWWtp8JB0bEvGcXF8f0DMv6RvL1jAbvp10jzt5Kiem7Qfne6EuvecVA==", databaseName: "Products");
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultContainer("Clothing");
+            //modelBuilder.Entity<Product>().HasPartitionKey(o => o.PartitionKey);
+        }
+    }
+}
---
+++ CosmosWithEF/CosmosWithEF/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace CosmosWithEF.Models
+{
+    public class Product
+    {
+        [JsonProperty("id")]
+      
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("price")]
+        public double Price { get; set; }
+
+        [JsonProperty("color")]
+        public string Color { get; set; }
+
+        [JsonProperty("size")]
+        public string Size { get; set; }
+
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+
+        
+    }
+}
